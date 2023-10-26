ba091aa5ae37efa784991273cc5be4a51a80ecb9
Alexis Lara <alexislaracamacho@my.whccd.edu>
2023-10-25 19:27:25 -0700

    added code to script.js and script1.js

--- script.js
+++ script.js
@@ -1,22 +1,19 @@
 //Tonight you will be demonstrating your ability to work with values, types, and operators. Create a new HTML document and link an external JavaScript file to it. Inside the JavaScript file, use console.log(...) to write out the result of the following operations:
 
 //8 * null = ???
-console.log("")
+console.log(8 * null)
 
-//
-console.log("")
+//“5” - 1 = ???
+console.log("5" - 1)
 
-//
-console.log("")
+//“5” + 1 = ???
+console.log("5" + 1)
 
-//
-console.log("")
+//“five” * 2 = ???
+console.log("five" * 2)
 
-//
-console.log("")
+//false == 0 = ???
+console.log(false == 0)
 
-//
-console.log("")
-
-//
-console.log("")
+//5 > 2 && 6 > 3 = ???
+console.log(5 > 2 && 6 > 3);
--- script1.js
+++ script1.js
@@ -1,7 +1,36 @@
+//single-line comment
+
+/*
+* this
+* is
+* a
+* multiple
+* line
+* comment
+*/
+
 //boolean example
 let variable1 = false;
-console.log("variable1");
+console.log(variable1);
 
 //null example
 let variable2 = null;
-console.log("variable2");
+console.log(variable2);
+
+//undefined example
+let variable3;
+console.log(variable3);
+
+//numbers
+let variable4 = 25;
+console.log(variable4);
+
+let variable5 = "25";
+console.log(variable5);
+
+let variable6 = 25.2e10;
+console.log(variable6);
+
+//strings
+let variable7 = "Hello World!";
+console.log(variable7);
